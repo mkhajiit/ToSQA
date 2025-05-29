ad828bff6d227a5462a9dcbe02d5bf7cb2a045fe
mkhajiit <mkhajiit15@gmail.com>
2025-05-29 21:36:27 +0900

    feat: 클릭, 요소 검색 기능 추가

--- Playwright/first-project/tests/firstTest.spec.ts
+++ Playwright/first-project/tests/firstTest.spec.ts
@@ -3,8 +3,9 @@ import test, { expect } from '@playwright/test';
 test('페이지 타이틀 확인', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
 
-  const title = await page.title();
-  console.log(title);
   await page.locator('#login-button').click();
-  expect(title).toBe('Swag Labs');
+  const text = await page.locator('.error-message-container.error').textContent();
+  expect(text).toBe('Epic sadface: Username is required');
+
+  await page.waitForTimeout(3000);
 });
