772fbc4e7d37c9f29a62b1d02430b83fc2428e9d
mkhajiit <mkhajiit15@gmail.com>
2025-05-31 20:49:56 +0900

    feat: beforeEach 적용

--- Playwright/first-project/tests/firstTest.spec.ts
+++ Playwright/first-project/tests/firstTest.spec.ts
@@ -1,11 +1,17 @@
 import test, { expect } from '@playwright/test';
 
-test('페이지 타이틀 확인', async ({ page }) => {
+// 파일 내 테스트들이 테스트 시작 전에 반드시 실행함
+test.beforeEach(async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
+  await page.fill('#user-name', 'standard_user');
+  await page.fill('#password', 'secret_sauce');
+  await page.click('#login-button');
+});
 
-  await page.locator('#login-button').click();
-  const text = await page.locator('.error-message-container.error').textContent();
-  expect(text).toBe('Epic sadface: Username is required');
+// 로그인 성공 확인
+test('로그인 성공 확인', async ({ page }) => {
+  // const text = await page.locator('.error-message-container.error').textContent();
+  await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
 
   await page.waitForTimeout(3000);
 });
