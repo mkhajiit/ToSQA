4406a54806d817a7c66cad7d8a1d3046f360681d
mkhajiit <mkhajiit15@gmail.com>
2025-06-07 20:51:17 +0900

    feat:map 복습

---
+++ TypeScript/cart-demo/cartDemo.ts
@@ -0,0 +1,19 @@
+interface IProduct {
+  id: number;
+  name: string;
+  price: number;
+}
+
+const myCart: IProduct[] = [
+  { id: 1, name: '사과', price: 1000 },
+  { id: 2, name: '바나나', price: 1500 },
+  { id: 3, name: '딸기', price: 2000 },
+];
+
+function printCartItemList(cart: IProduct[]) {
+  cart.map((item) => {
+    console.log(`id:${item.id} 이름:${item.name}, 가격:${item.price}`);
+  });
+}
+
+printCartItemList(myCart);
