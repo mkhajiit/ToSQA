4af6619f2c527e73962a86f2cc6fde794ad620a2
mkhajiit <mkhajiit15@gmail.com>
2025-06-08 20:27:28 +0900

    feat:filter 복습

--- TypeScript/cart-filter/cartFilter.ts
+++ TypeScript/cart-filter/cartFilter.ts
@@ -1,5 +1,13 @@
-interface IProduct {
-  id: number;
-  name: string;
-  price: number;
+import myCart, { IProduct } from '../constants/myCart';
+
+function printFilteredItemList(cart: IProduct[]) {
+  const filtered = cart.filter((item) => item.price >= 1500);
+  filtered.forEach((item) => console.log(`상품명: ${item.name}, 가격: ${item.price}원`));
 }
+
+function printFilteredItemSum(cart: IProduct[]) {
+  const filteredList = cart.filter((item) => item.price >= 1500);
+  console.log(filteredList.reduce((acc, item) => acc + item.price, 0));
+}
+printFilteredItemList(myCart);
+printFilteredItemSum(myCart);
