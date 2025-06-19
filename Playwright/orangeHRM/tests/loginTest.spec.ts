2ceac48c3891d448c9ae31c96a2f7b7324d38ae1
mkhajiit <mkhajiit15@gmail.com>
2025-06-19 21:33:51 +0900

    feat: 로그인 실패 추가

--- Playwright/orangeHRM/tests/loginTest.spec.ts
+++ Playwright/orangeHRM/tests/loginTest.spec.ts
@@ -3,7 +3,7 @@ import test, { expect } from '@playwright/test';
 test.beforeEach(async ({ page }) => {
   await page.goto('https://opensource-demo.orangehrmlive.com/');
 });
-test('로그인 테스트', async ({ page }) => {
+test('로그인 성공 테스트', async ({ page }) => {
   await page.fill('[name = "username"]', 'Admin');
   await page.fill('[name = "password"]', 'admin123');
   await page.click('.orangehrm-login-button');
@@ -13,4 +13,12 @@ test('로그인 테스트', async ({ page }) => {
   );
 
   await page.waitForTimeout(4000);
+});
+
+test('로그인 실패 테스트', async ({ page }) => {
+  await page.fill('[name = "username"]', 'Admin12');
+  await page.fill('[name = "password"]', 'admin12345');
+  await page.click('.orangehrm-login-button');
+
+  await expect(page.locator('.oxd-alert.oxd-alert--error')).toBeVisible();
 });
