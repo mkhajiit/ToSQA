50b41c078523b7af309be13d629d92cb780d1357
mkhajiit <mkhajiit15@gmail.com>
2025-07-03 16:12:08 +0900

    feat: map_filter_enumerate.py 마무리

--- Python/function/map_filter_enumerate.py
+++ Python/function/map_filter_enumerate.py
@@ -17,4 +17,20 @@ upper_cases = list(filter(str.isupper,text))
 
 print(upper_cases)
 
-# enumerate 주어진
+# enumerate()는 반복 가능한 객체(iterable)를 순회할 때,
+# 값 뿐 아니라 인덱스도 같이 얻을 수 있게 해주는 내장 함수
+# enumerate(iterable, start=0) start의 기본값은 0
+
+fruits = ["grape","banana","apple","strawberry","kiwi"]
+for i, fruit in enumerate(fruits, start=1):
+  print(f"인덱스 {i}번째 과일 {fruit}")
+
+# 보너스 zip
+# 여러 리스트를 병렬로 묶기
+# 인덱스가 같은 요소끼리 튜플로 묶인다
+names = ["철수", "영희", "민수"]
+scores = [90, 85, 78]
+
+home_lists = list(zip(names,scores))
+
+print(home_lists)
