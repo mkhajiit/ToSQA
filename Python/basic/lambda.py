b43fa2537952c0e6ab026dbe1403b588e4073fcf
mkhajiit <mkhajiit15@gmail.com>
2025-07-03 15:45:36 +0900

    feat:map 실습

---
+++ Python/basic/lambda.py
@@ -0,0 +1,4 @@
+add = lambda x,y: x+y
+minus = lambda x,y: x-y
+print(add(3,5))
+print(minus(5,3))
---
+++ Python/function/map_filter_zip.py
@@ -0,0 +1,7 @@
+numbers = [1,2,3,4,5,6]
+
+doubles = list(map(lambda x: x*2,numbers))
+squares = list(map(lambda x: x**2,numbers))
+
+print(doubles)
+print(squares)
