8cc8723addd5b0eef8f809f32c6baefe35c27f38
mkhajiit <mkhajiit15@gmail.com>
2025-07-06 16:23:49 +0900

    fix: 로그를 시간별로 따로 저장하도록 수정

--- Selenium/first/tests/conftest.py
+++ Selenium/first/tests/conftest.py
@@ -2,16 +2,19 @@
 import os
 import pytest
 import logging
+from datetime import datetime
 
 # 스크린샷, 로그 저장 폴더 생성
 os.makedirs("screenshots", exist_ok=True)
 os.makedirs("log", exist_ok=True)
 
+now = datetime.now().strftime("%Y%m%d_%H%M%S") # 지금 시간을 변수로 넣어서 설정함
 # 로거 설정
 logging.basicConfig(
-    filename='log/test.log',
+    filename= f'log/test_{now}.log',
     level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s'
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    encoding='utf-8' # 로그 저장시 한글 깨지는 걸 방지함
 )
 
 # 훅 함수에 추가 옵션을 주거나 명시적으로 표시할 때 쓴다.
@@ -33,7 +36,7 @@ def pytest_runtest_makereport(item, call):
             filename = f"screenshots/{item.name}_{browser_name}.png"
             driver.save_screenshot(filename)
             print(f"스크린샷 저장됨: {filename}")
-            logging.error(f"테스트 실패: {item.nodeid}")
-            logging.error(f"실패 메시지: {rep.longrepr}")
+            logging.error(f"테스트 실패: {item.nodeid}") # 어떤 테스트가 실패했는지
+            logging.error(f"실패 메시지: {rep.longrepr}") # AssertionError 메시지, 트레이스백(stack trace) 전체
         else:
             logging.error(f"테스트 '{item.name}' 실패! WebDriver 인스턴스를 찾을 수 없음.")
