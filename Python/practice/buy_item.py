ccb85523d32a028cc6d8dd17202259962bab1b50
mkhajiit <mkhajiit15@gmail.com>
2025-07-19 20:49:14 +0900

    fix:조건문 추가

--- Python/practice/buy_item.py
+++ Python/practice/buy_item.py
@@ -7,10 +7,13 @@ class buy_item:
     print(f"입력한 과일명은 {self.fruit}입니다.")
     print(f"입력한 보유금액은 {self.balance}입니다.")
 
+while True:
+  fruit = input("과일 이름을 입력하세요(포도,사과,키위,딸기): ")
+  balance = int(input("보유할 금액을 입력하세요: "))
+  status = buy_item(fruit,balance)
 
-fruit = input("과일 이름을 입력하세요(포도,사과,키위): ")
-balance = input("보유할 금액을 입력하세요: ")
-
-status = buy_item(fruit,balance)
-
-status.print_input()
+  if fruit in ["포도","사과","키위","딸기"] and balance > 0:
+    status.print_input()
+  else:
+    print("잘못된 입력 입니다!")
+    break
