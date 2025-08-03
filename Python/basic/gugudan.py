66be33d5bebf35fc8e8482373cbbbe5626b09599
mkhajiit <mkhajiit15@gmail.com>
2025-08-03 16:39:41 +0900

    fix:try except 추가

--- Python/basic/gugudan.py
+++ Python/basic/gugudan.py
@@ -1,7 +1,13 @@
-dan = int(input("구구단을 출력할 숫자를 입력하세요: "))
-
 def gugudan(dan):
   for i in range(1,10):
     print(f"{dan} x {i} = {dan * i}")
 
-gugudan(dan)
+
+try:
+  dan = int(input("구구단을 출력할 숫자를 입력하세요: "))
+  if dan < 1 or dan > 9:
+    raise ValueError("1부터 9 사이의 정수를 입력해야 합니다.") # 명시적으로 ValueError 를 발생시킵니다.
+  gugudan(dan)
+
+except ValueError as e:
+  print("입력 오류:",e)
